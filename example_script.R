352170f8fe478750c8a10be7b6aadccfbc21d1b4
rmichie <michie.ryan@deq.state.or.us>
2019-06-17 14:34:38 -0700

    Update example_script.R

--- example_script.R
+++ example_script.R
@@ -15,11 +15,15 @@ source(paste0(fun_dir,"/","import_stations_db.R"))
 
 
 # retreive AWQMS data
-df.awqms <- AWQMS_Data(startdate = "1995-01-01", enddate = "2019-12-31", 
+df.awqms <- AWQMS_Data(startdate = "1995-01-01", enddate = "2019-12-31",
            char = "Temperature, water",
-           HUC10 = "1801020604", 
+           HUC10 = "1801020604",
            crit_codes = TRUE,
            filterQC = TRUE)
+
+df.stations <- AWQMS_Stations(char = "Temperature, water",
+                              HUC8 = "18010206",
+                              crit_codes = TRUE)
 
 
 create_wq_db("Jenny_Creek.db")
