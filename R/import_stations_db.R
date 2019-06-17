daa7ef5ab4d2abaa042a43f2529af9c2195bb47f
rmichie <michie.ryan@deq.state.or.us>
2019-06-17 15:39:44 -0700

    Update import_stations_db.R
    fix long line length.

--- R/import_stations_db.R
+++ R/import_stations_db.R
@@ -1,3 +1,5 @@
+#'Import station info into a SQLite database.
+#'
 #' Take a vector of monitoring location station IDs (MLocIDs) and query station data from DEQ's Stations database. This station data is then loaded in the Stations table in the specified SQLite database. Note - trying to load in duplicate values will cause an error.
 #'
 #' @param mlocs Vector of unique monitoring location station IDs (MLocIDs) to retrieve from DEQ's Stations database.
@@ -5,11 +7,13 @@
 #' @param stations_db Stations database ODBC system data source name (DSN) identifed the ODBC data sources administrator. Default is "STATIONS".
 #' @keywords stations
 #' @export
+#' @return None
 #' @examples
 #' library(AWQMSdata)
 #'
-#' Retreive AWQMS data
-#' df.awqms <- AWQMS_Data(startdate = "1995-01-01", enddate = "2019-12-31",
+#' # Retreive AWQMS data
+#' df.awqms <- AWQMS_Data(startdate = "1995-01-01",
+#'                       enddate = "2019-12-31",
 #'                       char = "Temperature, water",
 #'                       HUC10 = "1801020604",
 #'                       crit_codes = TRUE,
@@ -17,8 +21,11 @@
 #'
 #'create_wq_db("Jenny_Creek.db")
 #'
-#'import_AWQMS_data(AWQMS_df=df.awqms, sqlite_db="Jenny_Creek.db")
-#'import_stations_db(mlocs=unique(df.awqms$MLocID), sqlite_db="Jenny_Creek.db", stations_db = "STATIONS")
+#'import_AWQMS_data(AWQMS_df=df.awqms,
+#'                  sqlite_db="Jenny_Creek.db")
+#'import_stations_db(mlocs=unique(df.awqms$MLocID),
+#'                   sqlite_db="Jenny_Creek.db",
+#'                   stations_db = "STATIONS")
 
 import_stations_db <- function(mlocs, sqlite_db, stations_db = "STATIONS"){
 
