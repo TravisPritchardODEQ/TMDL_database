ac37c2c50ea898d2c699daedeb85c4ec04710237
rmichie <michie.ryan@deq.state.or.us>
2019-06-17 10:59:20 -0700

    rename

--- R/import_stations_db.R
+++ R/import_stations_db.R
@@ -1,15 +1,19 @@
 
 
-insert_stations_db <- function(mlocs, sqlite_database, stations_database = "STATIONS"){
+import_stations_db <- function(mlocs, sqlite_db, stations_db = "STATIONS"){
+  
+  library(RSQLite)
+  library(DBI)
+  library(glue)
 
-con <- DBI::dbConnect(odbc::odbc(), stations_database)
+con <- DBI::dbConnect(odbc::odbc(), stations_db)
 query <- "Select * from VWStationsFinal where MLocID in ({mlocs*})"
 query <- glue::glue_sql(query,.con = con)
 stations <- DBI::dbGetQuery(con, query)
 DBI::dbDisconnect(con)
 
 
-con <- DBI::dbConnect(RSQLite::SQLite(), sqlite_database)
+con <- DBI::dbConnect(RSQLite::SQLite(), sqlite_db)
 DBI::dbWriteTable(con, 'Stations', value= stations, append = TRUE)
 DBI::dbDisconnect(con)
 
