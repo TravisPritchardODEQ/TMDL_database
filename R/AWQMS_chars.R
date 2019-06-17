f4aa4554e9cfb11a8e164cc0e51d5eda04b2d33d
rmichie <michie.ryan@deq.state.or.us>
2019-06-17 14:36:06 -0700

    Create AWQMS_chars.R
    function to load AWQMS_char.

---
+++ R/AWQMS_chars.R
@@ -0,0 +1,10 @@
+#' Table of AWQMS characters values.
+#'
+#' @docType data
+#' @usage data(AWQMS_chars)
+#' @keywords AWQMS
+#' @keywords datasets
+#' @examples
+#' data(AWQMS_chars)
+
+"AWQMS_chars"
