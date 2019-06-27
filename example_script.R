c62de825f11e76b56a1a6ce5f527fae15bbb38f4
rmichie <michie.ryan@deq.state.or.us>
2019-06-27 13:56:08 -0700

    Update example_script.R

--- example_script.R
+++ example_script.R
@@ -1,35 +1,44 @@
 
 library(devtools)
 
+devtools::install_github("rmichie/wqdb", host = "https://api.github.com")
 devtools::install_github("TravisPritchardODEQ/AWQMSdata", host = "https://api.github.com")
 
+library(wqdb)
 library(AWQMSdata)
 
-fun_dir <- "E:/GitHub/wq_sqlite/R"
+# tations database ODBC system data source name (DSN) identifed the ODBC data sources administrator. Default is usually "STATIONS".
+stations_odbc <- "STATIONS"
 
-setwd("E:/GitHub/wq_sqlite")
+setwd("E:/GitHub/wqdb/example")
 
-source(paste0(fun_dir,"/","create_wq_db.R"))
-source(paste0(fun_dir,"/","import_AWQMS_data.R"))
-source(paste0(fun_dir,"/","import_stations_db.R"))
-
+df.stations <- query_stations(stations_odbc=stations_odbc)
 
 # retreive AWQMS data
-df.awqms <- AWQMS_Data(startdate = "1995-01-01", enddate = "2019-12-31",
+df.awqms <- AWQMS_Data(startdate = "1995-01-01", enddate = "2000-12-31",
            char = "Temperature, water",
            HUC10 = "1801020604",
            crit_codes = TRUE,
            filterQC = TRUE)
 
-df.stations <- AWQMS_Stations(char = "Temperature, water",
-                              HUC8 = "18010206",
-                              crit_codes = TRUE)
+# Get station IDs
+mlocs <- unique(df.awqms$MLocID)
 
+# retreive staiton info from stations database
+con <- DBI::dbConnect(odbc::odbc(), stations_odbc)
+query <- "Select * from VWStationsFinal where MLocID in ({mlocs*})"
+query <- glue::glue_sql(query,.con = con)
+df.stations <- DBI::dbGetQuery(con, query)
+DBI::dbDisconnect(con)
 
-create_wq_db("Jenny_Creek.db")
+# create and write data into the db
+write_wqdb(db="Jenny_Creek.db",awqms=df.awqms, stations=df.stations)
 
-import_AWQMS_data(AWQMS_df=df.awqms, sqlite_db="Jenny_Creek.db")
-import_stations_db(mlocs=unique(df.awqms$MLocID), sqlite_db="Jenny_Creek.db", stations_db = "STATIONS")
+# get all the data back
+df <- read_wqdb("Jenny_Creek.db")
+
+unlink("Jenny_Creek.db")
+
 
 
 
