318fcb6cf881ea6333ef2c1b7655be3669776cf3
rmichie <michie.ryan@deq.state.or.us>
2019-07-09 20:39:22 -0700

    Update example_script.R

--- example_script.R
+++ example_script.R
@@ -12,24 +12,18 @@ stations_odbc <- "STATIONS"
 
 setwd("E:/GitHub/wqdb/example")
 
-df.stations <- query_stations(stations_odbc=stations_odbc)
+df.stations <- query_stations(stations_odbc=stations_odbc, huc10 = "1801020604")
+
+# Get station IDs
+mlocs <- unique(df.stations$MLocID)
 
 # retreive AWQMS data
-df.awqms <- AWQMS_Data(startdate = "1995-01-01", enddate = "2000-12-31",
+df.awqms <- AWQMS_Data(startdate = "1995-01-01", enddate = "2002-12-31",
            char = "Temperature, water",
-           HUC10 = "1801020604",
+           station = mlocs,
            crit_codes = TRUE,
            filterQC = TRUE)
 
-# Get station IDs
-mlocs <- unique(df.awqms$MLocID)
-
-# retreive staiton info from stations database
-con <- DBI::dbConnect(odbc::odbc(), stations_odbc)
-query <- "Select * from VWStationsFinal where MLocID in ({mlocs*})"
-query <- glue::glue_sql(query,.con = con)
-df.stations <- DBI::dbGetQuery(con, query)
-DBI::dbDisconnect(con)
 
 # create and write data into the db
 write_wqdb(db="Jenny_Creek.db",awqms=df.awqms, stations=df.stations)
