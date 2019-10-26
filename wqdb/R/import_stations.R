a4e5051ab8a2dd3c31622cf3e2dd876059506027
rmichie <michie.ryan@deq.state.or.us>
2019-10-25 17:55:17 -0700

    Update import_stations.R
    Updated datatypes and additinal fields to be consistent with AWQMS.

--- wqdb/R/import_stations.R
+++ wqdb/R/import_stations.R
@@ -19,9 +19,6 @@ import_stations <- function(file, huc_shp_dir, huc_shp_name) {
 
   # Setup -------------------------------------------------------------------
 
-  # clean out exisiting environment
-  # helps to avoid overwriting
-
   huc_shp <- rgdal::readOGR(dsn=huc_shp_dir, layer=huc_shp_name, stringsAsFactors=FALSE, integer64="warn.loss")
 
   huc_shp <- sp::spTransform(huc_shp, sp::CRS("+proj=longlat +datum=NAD83"))
@@ -31,8 +28,8 @@ import_stations <- function(file, huc_shp_dir, huc_shp_name) {
   # Import Locations Info -------------------------------------------------------------------
 
   locations_col_types <- c('text', 'text', 'text', 'numeric', 'numeric', 'text', 'text', 'text', 'text', 'text',
-                           'text', 'text', 'text', 'numeric', 'date', 'text', 'text', 'text', 'text', 'text',
-                           'text', 'text', 'text', 'text', 'numeric', 'numeric', 'numeric', 'numeric')
+                           'text', 'text', 'text', 'text', 'date', 'text', 'text', 'text', 'text', 'text',
+                           'text', 'text', 'text', 'text', 'text', 'numeric', 'text', 'numeric')
 
   locations_import <- readxl::read_excel(file, sheet = "Monitoring_Locations", col_types = locations_col_types)
 
@@ -41,29 +38,49 @@ import_stations <- function(file, huc_shp_dir, huc_shp_name) {
   df.mloc1 <- locations_import %>%
     dplyr::select(MLocID=Monitoring.Location.ID,
                   StationDes=Monitoring.Location.Name,
-                  MonLocType=Monitoring.Location.Type,
-                  HUC8=HUC.8.Code,
                   Lat_DD=Latitude,
                   Long_DD=Longitude,
+                  CollMethod=Coordinate.Collection.Method,
+                  Datum=Horizontal.Datum,
+                  MapScale=Source.Map.Scale,
+                  AU_ID=Assessment.Unit.ID,
+                  MonLocType=Monitoring.Location.Type,
+                  TribalLand=Tribal.Land.,
+                  TribalName=Tribal.Land.Name,
+                  AltLocID=Alternate.ID.1,
+                  AltLocName=Alternate.Context.1,
+                  Comments=Monitoring.Location.Comments,
+                  STATE=State.Code,
+                  COUNTY=County.Name,
+                  HUC8=HUC.8.Code,
+                  GNIS_Name,
                   Reachcode,
                   Measure,
-                  AU_ID=Assessment.Unit.ID) %>%
-    dplyr::mutate(EcoRegion3=as.character(NA),
-                  EcoRegion4=as.character(NA))
+                  LLID,
+                  RiverMile) %>%
+    dplyr::mutate(TribalLand=ifelse(grepl("[Yy]es",TribalLand),1,0))
 
+  # make spatial
   sp::coordinates(df.mloc1) <- ~Long_DD+Lat_DD
   df.mloc1@proj4string <- huc_shp@proj4string
 
+  # Extract HUC info
   mlocs_in_hucs <- sp::over(df.mloc1, huc_shp)
 
-  # Extract HUC info
+  # add to dataframe
   df.mloc2 <- mlocs_in_hucs %>%
     dplyr::select(HUC8_Name=HU_8_NAME, HUC10=HUC_10, HUC10_Name=HU_10_NAME, HUC12=HUC_12, HUC12_Name=HU_12_NAME) %>%
     cbind(as.data.frame(df.mloc1)) %>%
-    dplyr::select(MLocID, StationDes, MonLocType, EcoRegion3, EcoRegion4,
-                  HUC8, HUC8_Name, HUC10, HUC10_Name, HUC12, HUC12_Name,
-                  Lat_DD, Long_DD, Reachcode, Measure, AU_ID)
+    dplyr::mutate(HUC10=as.character(HUC10),
+                  HUC12=as.character(HUC12))
 
-  return(df.mloc2)
+  # Add other cols that are not in xlsx template
+  df.mloc2[ , setdiff(wqdb::station_cols(), colnames(df.mloc2))] <- NA
+
+  # reorder
+  df.mloc3 <- df.mloc2 %>%
+    dplyr::select(wqdb::station_cols())
+
+  return(df.mloc3)
 
 }
